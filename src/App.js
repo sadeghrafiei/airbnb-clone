c617d9fa1bbeae1278cd6397597ae73552346cf1
sadeghrafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-03-11 22:36:11 +0330

    add Home file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div className="app">
      <h1>lets build airbnb clone</h1>
+     <Home />
 
      {/* Header */}
 
      {/* Banner */}
+
+     {/* Cards */}
+
+     {/* Footer */}
+
+     {/* SearchPage */}
+
+     {/* .... */}
     </div>
   );
 }
