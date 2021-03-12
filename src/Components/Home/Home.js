0ea02259f172c8c98146077f32d120aa9b0f871b
sadeghrafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-03-12 14:27:33 +0330

    add card and start to build

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,6 +6,17 @@ function Home() {
     return (
         <div className="home"> 
             <Banner />
+
+            <div className="home__section">
+                <Card />
+                <Card />
+                <Card />
+            </div>
+            <div className="home__section">
+                <Card />
+                <Card />
+                <Card />
+            </div>
         </div>
     )
 }
