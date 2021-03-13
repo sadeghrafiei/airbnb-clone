fa23b45bdfca1049167ccf58d80a66aac324e448
sadeghrafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-03-13 12:31:44 +0330

    add react-router-dom to components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import "./App.css";
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
