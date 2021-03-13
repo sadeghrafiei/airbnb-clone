65a00b20a44f6293ace1e2194c83431aa0d5a1f1
sadeghrafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-03-13 12:33:06 +0330

    create search page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SearchPage from "./Components/SearchPage/SearchPage";
 
 function App() {
   return (
